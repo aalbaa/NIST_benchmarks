c5514bdf23f19775fca1833e72153609fb685068
albaalia <albaalia@live.com>
2020-08-01 02:04:02 -0400

    Able to read parameters from the file

--- NIST_Parser.cpp
+++ NIST_Parser.cpp
@@ -14,6 +14,11 @@ const string kIntParams_arr[] = {"Starting Values", "Certified Values", "Data ",
 " Response", " Predictor", " Observations", " Parameters", 
 "Degrees of Freedom", "Number of Observations"};
 
+// Array of string names of the residual parameters
+const string kDoubleParams_arr[] =  {"Residual Sum of Squares", "Residual Standard Deviation"};
+// Number of the 'double' type parameters (residuals)
+const int kNum_doubleParams = 2;
+
 
 class NIST_Parser{
 
@@ -23,6 +28,12 @@ class NIST_Parser{
 
             // Number of integer parameters
             num_intParams_ = kNum_intParams;
+
+            // Number of double parameters
+            num_doubleParams_ = kNum_doubleParams;
+
+            // Array of residual errors
+            residual_arr_ = new double[num_doubleParams_];
 
             // Line numbers of the integer parameters
             line_numbers_intParams_ = new int[num_intParams_];
@@ -41,6 +52,7 @@ class NIST_Parser{
 
             // Delete dynamic arrays
             delete[] line_numbers_intParams_;
+            delete[] residual_arr_;
         }
 
         void OpenFile(string file_name){
@@ -60,9 +72,13 @@ class NIST_Parser{
             return file_stream_.open(file_name_);
         }
 
+        // TEMPORARY! This function will not be used later
         int* line_numbers_intParams(){return line_numbers_intParams_;};
-        void ReadParameters(){
-            if(values_parsed_){
+
+        void ParseParameterLines(){
+            // This function only gets the parameter lines (the integer parameters)
+
+            if(param_lines_parsed_){
                 return;
             }
             if(!file_stream_.is_open()){
@@ -78,31 +94,60 @@ class NIST_Parser{
             // Integer keeping track of line numbers. First line is 1 (not zero)
             int line_number = 0;
             // Number of strings found so far
-            int num_strings_found = 0;
-            while(getline(file_stream_, input_line) && num_strings_found < num_intParams_){
+            int num_intParams_found = 0;
+            // Number of 'double parameters' found
+            int num_doubleParams_found = 0;
+            while(getline(file_stream_, input_line) && num_intParams_found < num_intParams_){
                 line_number++;
                 // Get starting values
-                found_str = input_line.find(kIntParams_arr[num_strings_found]);
+                found_str = input_line.find(kIntParams_arr[num_intParams_found]);
                 if(found_str != string::npos){
-                    GetLineNumbers(input_line, line_numbers_intParams_ + num_strings_found, 1);
-                    num_strings_found++;
+                    GetLineNumbers(input_line, line_numbers_intParams_ + num_intParams_found, 1);
+                    num_intParams_found++;
                     continue;
                 }
-                // // Otherwise, look for the residual values
-                // found_str = line.find(str_residual[idx_str_params_double]);
-                // if(found_str != string::npos){
-                //     // Add value to the array address so to store parameters in the appropriate element locations
-                //     get_line_numbers(line,  residual_arr+idx_str_params_double, 1);
-                //     idx_str_params_double++;
-                //     continue;
-                // }
+                // Otherwise, look for the residual values
+                found_str = input_line.find(kDoubleParams_arr[num_doubleParams_found]);
+                if(found_str != string::npos){
+                    // Add value to the array address so to store parameters in the appropriate element locations
+                    GetLineNumbers(input_line,  residual_arr_ + num_doubleParams_found, 1);
+                    num_doubleParams_found++;
+                    continue;
+                }
             }
             
+            // Number of parameters
+            num_params_ = line_numbers_intParams_[6];
+            // Number of inputs (predictor)
+            num_input_ = line_numbers_intParams_[4];
+            // Number of outputs (response)
+            num_output_ = line_numbers_intParams_[3];
+            // Number of observations
+            num_observations_ = line_numbers_intParams_[5];
 
             // Flip the flag if parameters successfully read
-            values_parsed_ = true;
+            param_lines_parsed_ = true;
         }
 
+        int num_params(){
+            if(!param_lines_parsed_) ParseParameterLines();
+            return num_params_;
+        }
+
+        int num_input(){
+            if(!param_lines_parsed_) ParseParameterLines();
+            return num_input_;
+        }
+        
+        int num_output(){
+            if(!param_lines_parsed_) ParseParameterLines();
+            return num_output_;
+        }
+
+        int num_observations(){
+            if(!param_lines_parsed_) ParseParameterLines();
+            return num_observations_;
+        }
     private:
         void GetLineNumbers(string str, double * vals, int num_vals){
             // Get a line number from a string.
@@ -169,6 +214,7 @@ class NIST_Parser{
         // ***************************
         // Flags
         // Flag indicating whether parameters have been read or not
+        bool param_lines_parsed_ = false;
         bool params_parsed_ = false;
         bool values_parsed_ = false;
 
@@ -178,19 +224,24 @@ class NIST_Parser{
         // Number of integer parameters
         int num_intParams_;
         // Indices of line numbers
-        int *line_numbers_intParams_;
+        int* line_numbers_intParams_;
+
+        // Number of double parameters
+        int num_doubleParams_;
+        double* residual_arr_;
 };
 
 int main(){
     string file_name = "Chwirut2_dat.txt";
     NIST_Parser nist_parser(file_name);
-    nist_parser.ReadParameters();
-    nist_parser.ReadParameters();
+    nist_parser.ParseParameterLines();
 
     int* line_intParams = nist_parser.line_numbers_intParams();
     // Print the parameter values
     for(int i=0; i<kNum_intParams; i++){
         cout << line_intParams[i] << endl;
     }
+
+    cout << "Number of observations: " << nist_parser.num_observations() << endl;
 }
 
