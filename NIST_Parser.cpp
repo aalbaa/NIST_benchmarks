fc8c07da622fdf4212f2b5649a3806f5e146c783
albaalia <albaalia@live.com>
2020-08-13 23:59:10 -0400

    Included example in Playground to convert the data into matrices

--- NIST_Parser.cpp
+++ NIST_Parser.cpp
@@ -350,19 +350,20 @@ class NIST_Parser{
         double** data_vals_;
 };
 
-int main(){
-    std::string file_name = "Chwirut2_dat.txt";
-    NIST_Parser nist_parser(file_name);
+// int main(){
+//     std::string file_name = "Chwirut2_dat.txt";
+//     NIST_Parser nist_parser(file_name);
 
-    double** data_vals = nist_parser.data_vals();
-    int num_observations = nist_parser.num_observations();
+//     double** data_vals = nist_parser.data_vals();
+//     int num_observations = nist_parser.num_observations();
 
-    // Print the parameter values
-    for(int i = 0; i < num_observations; i++){
-        for(int j = 0; j < 2; j++){
-            std::cout << data_vals[i][0] << "\t" << 
-                data_vals[i][1] << std::endl;
-        }
-    }
-}
+//     // Print the parameter values
+//     for(int i = 0; i < num_observations; i++){
+//         for(int j = 0; j < 2; j++){
+//             std::cout << data_vals[i][0] << "\t" << 
+//                 data_vals[i][1] << std::endl;
+//         }
+//     }
+//     std::cout << "Hello thereeeeeNo" << std::endl;
+// }
 
--- playground.cpp
+++ playground.cpp
@@ -1,24 +1,80 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <Eigen/Dense>
 
 #include "NIST_Parser.cpp"
 
 using namespace std;
+using namespace Eigen;
+
+MatrixXd getOutputMatrix(double**, int, int);
+MatrixXd getInputMatrix(double**, int, int, int);
 
 int main(){
     string file_name = "Chwirut2_dat.txt";
     NIST_Parser nist_parser(file_name);
 
     double** data_vals = nist_parser.data_vals();
+
+    // // Matrix of the output values
+    // MatrixXd outputMatrix(nist_parser.num_observations(), nist_parser.num_output());
+    // // Matrix of input values
+    // MatrixXd inputMatrix(nist_parser.num_observations(), nist_parser.num_input());
+
+    // nist_parser.
+    // Convert the double array into a Matrix
     int num_observations = nist_parser.num_observations();
 
-    // Print the parameter values
+    // // Print the parameter values
+    // for(int i = 0; i < num_observations; i++){
+    //     // First, the outputs
+    //     for(int j = 0; j < nist_parser.num_output(); j++){
+    //         outputMatrix(i, j) = data_vals[i][j];
+    //     }
+    //     // Now, the inputs
+    //     for(int j = 0; j < nist_parser.num_input(); j++){
+    //         inputMatrix(i, j) = data_vals[i][nist_parser.num_output() + j];
+    //     }
+    // }
+    MatrixXd outputMatrix = getOutputMatrix(data_vals, nist_parser.num_observations(), 
+            nist_parser.num_output());
+    MatrixXd inputMatrix = getInputMatrix(data_vals, nist_parser.num_observations(), 
+            nist_parser.num_output(), nist_parser.num_input());
+
+    cout << "Output matrix: " << endl;
+    cout << outputMatrix << endl;
+    cout << "Input matrix: " << endl;
+    cout << inputMatrix << endl;
+}
+
+
+MatrixXd getOutputMatrix(double** data_vals, int num_observations, int num_output){
+    // Function to convert the double array into matrix. 
+    // This function returns the output values
+
+    // Matrix of the output values
+    MatrixXd outputMatrix(num_observations, num_output);
     for(int i = 0; i < num_observations; i++){
-        for(int j = 0; j < 2; j++){
-            cout << data_vals[i][0] << "\t" << 
-                data_vals[i][1] << endl;
+        // First, the outputs
+        for(int j = 0; j < num_output; j++){
+            outputMatrix(i, j) = data_vals[i][j];
         }
     }
+    return outputMatrix;
+}
 
+MatrixXd getInputMatrix(double** data_vals, int num_observations, int num_output, int num_input){
+    // Function to convert the double array into matrix. 
+    // This function returns the input values
+
+    // Matrix of the output values
+    MatrixXd inputMatrix(num_observations, num_input);
+    for(int i = 0; i < num_observations; i++){
+        // First, the outputs
+        for(int j = 0; j < num_input; j++){
+            inputMatrix(i, j) = data_vals[i][num_output + j];
+        }
+    }
+    return inputMatrix;
 }
